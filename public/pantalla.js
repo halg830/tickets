d44504407b438f986b4fd83ad2b8aefb6d6ff755
halg830 <halg830@gmail.com>
2023-09-20 00:58:23 -0500

    pantalla funcional

--- public/pantalla.js
+++ public/pantalla.js
@@ -1,24 +1,32 @@
-const socket = io()
+const socket = io();
 
-const div = document.querySelector("#contNumeros")
-const numeros = []
-socket.on("informarTicket", (num)=>{
-    numeros.push(num)
-})
+const div = document.querySelector("#contNumeros");
+let ticket = 0
 
-let fragment = document.createDocumentFragment()
-let contador = 0
-let numero = numeros[numeros.length-1]+1
-for(let i=0; i<numeros.length-1; i++){
-    if(contador==4) break
-    else contador+=1
+socket.on("connect", () => {
+  console.log("En linea");
+});
 
-    const h1 = document.createElement("h1")
-    
-    numero-=1
-    h1.textContent=numero
-    fragment.appendChild(h1)
-}
-div.appendChild(fragment)
+socket.on("informarTicket", (num) => {
+   ticket = num
+  actualizarNumeros()
+});
 
+function actualizarNumeros (){
+    div.innerHTML=""
+    let fragment = document.createDocumentFragment();
+    let contador = 0;
+    let numero = ticket+1
 
+    for (let i = 0; i < 4; i++) {
+      if (contador == 4 || numero<=1) break;
+      else contador += 1;
+  
+      const h1 = document.createElement("h1");
+  
+      numero -= 1;
+      h1.textContent = numero;
+      fragment.appendChild(h1);
+    }
+    div.appendChild(fragment);
+  };
